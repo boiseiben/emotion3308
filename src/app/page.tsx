82083473ebc410a33bd5901f7ad40b0b67cf164b
boiseiben <202329030+boiseiben@users.noreply.github.com>
2025-03-11 10:25:33 -0400

    Add button on page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
+import Button from '@mui/material/Button';
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        {/* You can get rid of both the error (and the button) by moving the contents of 
+        the next line to the end of the line below it.
+        */}
+        <Button>Problem Causer</Button>
         <Image
           className="dark:invert"
           src="/next.svg"
